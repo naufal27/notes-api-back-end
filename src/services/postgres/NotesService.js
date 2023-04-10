8210d2529eb0be86dd01289034401ea8158031d9
naufal27 <mnaufalr27@gmail.com>
2023-04-11 00:22:41 +0700

    collaboration implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -6,8 +6,24 @@ const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthorizationError = require('../../exceptions/AuthorizationError');
  
 class NotesService {
-  constructor() {
+  constructor(collaborationService) {
     this._pool = new Pool();
+    this._collaborationService = collaborationService;
+  }
+
+  async verifyNoteAccess(noteId, userId) {
+    try {
+      await this.verifyNoteOwner(noteId, userId);
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error;
+      }
+      try {
+        await this._collaborationService.verifyCollaborator(noteId, userId);
+      } catch {
+        throw error;
+      }
+    }
   }
 
   async addNote({ title, body, tags, owner }) {
@@ -31,7 +47,10 @@ class NotesService {
 
   async getNotes(owner) {
     const query = {
-      text: 'SELECT * FROM notes WHERE owner = $1',
+      text: `SELECT notes.* FROM notes
+        LEFT JOIN collaborations ON collaborations.note_id = notes.id
+        WHERE notes.owner = $1 OR collaborations.user_id = $1
+        GROUP BY notes.id`,
       values: [owner],
     };
     const result = await this._pool.query(query);
@@ -40,15 +59,18 @@ class NotesService {
 
   async getNoteById(id) {
     const query = {
-      text: 'SELECT * FROM notes WHERE id = $1',
+      text: `SELECT notes.*, users.username
+      FROM notes
+      LEFT JOIN users ON users.id = notes.owner
+      WHERE notes.id = $1`,
       values: [id],
     };
     const result = await this._pool.query(query);
- 
+   
     if (!result.rows.length) {
       throw new NotFoundError('Catatan tidak ditemukan');
     }
- 
+   
     return result.rows.map(mapDBToModel)[0];
   }
   
